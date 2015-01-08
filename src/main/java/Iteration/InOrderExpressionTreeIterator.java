9a1478d212a4e6efa428e5ce9faa6e71d71537d1
Michael King <michaeldigit@gmail.com>
2015-01-08 12:03:13 -0800

    Updated InOrderExpressionTreeIterator
    Now returns parethesis as leaf nodes during iteration. The leaf node is
    just a LeafNode instance with an '(' or ')' item.

--- src/main/java/Iteration/InOrderExpressionTreeIterator.java
+++ src/main/java/Iteration/InOrderExpressionTreeIterator.java
@@ -1,50 +1,82 @@
 import java.util.*;
 
 public class InOrderExpressionTreeIterator implements Iterator<ComponentNode>{
-	
-	List<ComponentNode> traversalList;
-	ComponentNode currentNode;
-	
-	private InOrderExpressionTreeInterator(){}
 
+	// TODO This class must some how add parenthesis	
+	List<ComponentNode> pendingList;
+	ComponentNode current;
+	
+	private InOrderExpressionTreeIterator(){}
+	
 	public InOrderExpressionTreeIterator(ExpressionTree expressionTree){
-		traversalList = new ArrayList<ComponentNode>();
-		currentNode = null;
-		if (expressionTree != null){
-			traverseLeft(expressionTree);
-		}		
+		pendingList = new ArrayList<ComponentNode>();
+		current = expressionTree;
+		goLeft();
 	}
 
 	public boolean hasNext(){
-		if (currentNode != null)
+		if (current != null)
 			return true;
 		return false;
 	}
 	
 	public ComponentNode next(){
-		result = currentNode;
-		componentNode temp = currentNode.getRightNode();
-		if( temp != null){
-			traverseLeft(temp);
-		} else {
-			int last = traversalList.length() - 1;
-			currentNode - traversalList.get(last);
-
-		}
+		ComponentNode result = current;
+		if(current.getRightChild() != null)
+			goRight();
+		else
+			current = pendingList.remove(0);
 		return result;
 	}
 	
-	private void traverseLeft(ComponentNode node){
-		ComponentNode temp = node.getLeftNode();
-		while (temp != null){
-			traversalList.add(node);
-			node = temp;
-			temp = node.getLeftNode();			
-		}
-		currentNode = node;
+	private void goRight(){
+		current = current.getRightChild();
+		if (current.getLeftChild() != null)
+			goLeft();
 	}
 	
+	private void goLeft(){
+		while (current != null){
+			// if has a left child, add the ( ) to pending when needed
+			boolean hasLeftChild = 
+				(current.getLeftChild() != null) ? true: false;
+			if (hasLeftChild){
+				int rightParPos = lastIndexOfRightParenthesis() + 1; 
+				pendingList.add(rightParPos, new LeafNode(")"));
+			}
+			// add the current to pending
+			int curPos = lastIndexOfRightParenthesis() + 1;
+			pendingList.add(curPos, current);
+			// if has a left child the the ( to pending after.
+			if (hasLeftChild){
+				int leftParPos = lastIndexOfRightParenthesis() + 1; 
+				pendingList.add(leftParPos, new LeafNode("("));
+			}
+			// next
+			current = current.getLeftChild();
+		}
+		// if there is any pending, current is the first pending.
+		if (pendingList.size() > 0)
+			current = pendingList.remove(0); // remove it!
+	}
 	
-	
-	
+	private int lastIndexOfRightParenthesis(){
+		int result = -1;
+		int pendingListSize = pendingList.size(); 
+		if (pendingListSize == 0){
+			return result;
+		}
+		int index = 0;
+		// get the first as next for entrance into a while loop
+		ComponentNode next = pendingList.get(index);
+		while(next.getItem().equals("(")){
+			result = index;
+			index += 1;
+			if (index  >= pendingListSize){
+				return result;
+			}			
+			next = pendingList.get(index);		
+		}
+		return result;
+	}		
 }
